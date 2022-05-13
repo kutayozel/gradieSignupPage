83dfc88236825708bbd1d8361270ab178fb7e10b
kutayozel <zelkutay@gmail.com>
2022-05-13 22:41:26 +0300

    css

---
+++ src/Login/Login.js
@@ -0,0 +1,31 @@
+import './loginrwo.scss'
+import Mockup from '../imgs/Mockup.png'
+
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <div className="headerImg">
+        <img src={Mockup} alt="img" />
+      </div>
+      <div className="loginForm">
+        <div className="header">Login</div>
+        <form className='forms'>
+          <label htmlFor="email">Email</label>
+          <input type="text" name='email' placeholder='john@example@gmail.com' />
+          <label htmlFor="password">Password</label>
+          <input type="text" name='password' placeholder='At least 8 characters' />
+        </form>
+        <div className="terms">
+          <input type="radio" />
+          <div> By creating an account, you agree to the <a href="#">Terms & Conditions.</a> </div>
+        </div>
+
+        <button className='create'>Create an Account</button>
+      </div>
+    </div>
+
+  )
+}
+
+export default Login
