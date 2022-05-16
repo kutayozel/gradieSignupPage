29f8d09c63b79f697927501bab5412aa51b9ffde
kutayozel <zelkutay@gmail.com>
2022-05-16 17:15:39 +0300

    sec

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -8,21 +8,24 @@ const Login = () => {
       <div className="headerImg">
         <img src={Mockup} alt="img" />
       </div>
-      <div className="loginForm">
-        <div className="header">Login</div>
-        <form className='forms'>
-          <label htmlFor="email">Email</label>
-          <input type="text" name='email' placeholder='john@example@gmail.com' />
-          <label htmlFor="password">Password</label>
-          <input type="text" name='password' placeholder='At least 8 characters' />
-        </form>
-        <div className="terms">
-          <input type="radio" />
-          <div> By creating an account, you agree to the <a href="#">Terms & Conditions.</a> </div>
-        </div>
+      <div className='bottomPart'>
+        <div className="loginForm">
+          <div className="header">Login</div>
+          <form className='forms'>
+            <label htmlFor="email">Email</label>
+            <input type="text" name='email' placeholder='john@example@gmail.com' />
+            <label htmlFor="password">Password</label>
+            <input type="text" name='password' placeholder='At least 8 characters' />
+          </form>
+          <div className="terms">
+            <input type="radio" />
+            <div> By creating an account, you agree to the <a href="#">Terms & Conditions.</a> </div>
+          </div>
 
-        <button className='create'>Create an Account</button>
+          <button className='create'>Create an Account</button>
+        </div>
       </div>
+
     </div>
 
   )
